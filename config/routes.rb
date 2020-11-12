405372689bb086bd7d4d02a7812671bfe5abfe88
Elsa Lebas <lebas.elsa@gmail.com>
2020-11-12 16:35:39 +0100

    implement back end of app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
+
+# Exemple for resto and reviews
+# resources :restaurants, only: [:new, :create, :index, :show] do
+  # resources :reviews, only: [:new, :create]
+# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-# puts 'Creating 2 fake cocktails...'
-#   pina = Cocktail.new(name: "Pina Colada")
-#   pina.save!
-#   marga = Cocktail.new(name: "Margarita")
-#   marga.save!
-# puts 'Finished!'
+puts 'Creating 2 fake cocktails...'
+  pina = Cocktail.new(name: "Pina Colada")
+  pina.save!
+  marga = Cocktail.new(name: "Margarita")
+  marga.save!
+puts 'Finished!'
 
 puts 'Creating 10 fake ingredients...'
 10.times do
@@ -11,3 +11,7 @@ puts 'Creating 10 fake ingredients...'
   ingredient.save!
 end
 puts 'Finished!'
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
